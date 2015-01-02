fc1139d2873f38f05a71522891fd90af6a83f8ee
Ethan Durrant <emd519@yahoo.com>
2015-01-01 18:26:44 -0800

    made dir for lpc812 stuff and copied blink program into it

---
+++ lpc8xx/mcus/lpc812/blink/main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/*
+ * 	datasheet: http://www.nxp.com/documents/data_sheet/LPC81XM.pdf
+ *  user manual: http://www.nxp.com/documents/user_manual/UM10601.pdf
+ *  swtich matrix tool: http://www.lpcware.com/content/nxpfile/nxp-switch-matrix-tool-lpc800 (for pin configuration)
+ *
+ *
+ *  ISP: When pin PIO0_1 is pulled LOW on reset, the part enters ISP mode and the ISP command handler starts up. 
+ *       In ISP mode, pin PIO0_0 is connected to function U0_RXD and pin PIO0_4 is connected to function U0_TXD on the USART0 block.
+ */
+
+volatile uint32_t msTicks = 0;
+
+void SysTick_Handler(void) {
+	msTicks++;
+}
+
+void delay_ms(uint32_t ms) {
+	uint32_t now = msTicks;
+	while ((msTicks-now) < ms);
+}
+
+#define LED1 9        
+#define LED2 11        
+
+int main(void) {
+
+	SysTick_Config(SystemCoreClock / 1000);
+
+
+	LPC_GPIO_PORT->DIR0 |= (1<<LED1)|(1<<LED2);    // set pins as output
+
+	while (1)
+	{
+		LPC_GPIO_PORT->SET0 = 1<<LED1;    // LED1 output high
+		LPC_GPIO_PORT->CLR0 = 1<<LED2;    // LED2 output low
+		delay_ms(1000);
+		LPC_GPIO_PORT->CLR0 = 1<<LED1;    // LED1 output low
+		LPC_GPIO_PORT->SET0 = 1<<LED2;    // LED2 output high
+		delay_ms(1000);
+	}
+
+}
+
